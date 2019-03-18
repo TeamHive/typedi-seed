d3e59f72ec37a20deb85176db5ded35ab0aeccb0
Tyler Vander Maas <tvmaasjazz@gmail.com>
2019-03-18 17:51:28 -0400

    chore(email): Remove email

--- src/modules/app.service.ts
+++ src/modules/app.service.ts
@@ -1,37 +1,25 @@
 import { ErrorHandler } from '@teamhive/typedi-common';
 import { Service } from 'typedi';
-import { EmailService } from './common/email/services/email/email.service';
 
 @Service()
 export class AppService {
     constructor(
-        private readonly emailService: EmailService,
         private readonly errorHandler: ErrorHandler
     ) {}
 
-    async run(): Promise<void> {
+    async run() {
         try {
             const startTime = Date.now();
             this.errorHandler.captureBreadcrumb({ message: 'Running TypeDi Seed...' });
 
             // do things in services...
 
-            // send report
-            const emailSent = await this.emailService.sendExampleEmail();
-
-            // soft delete all of those alerts if sent successfully
-            if (emailSent) {
-                this.errorHandler.captureBreadcrumb({ message: 'Successfully sent example email.' });
-            }
-            else {
-                this.errorHandler.captureBreadcrumb({ message: 'Example email not sent.' });
-            }
-
             this.errorHandler.captureBreadcrumb({ message: `Finished TypeDi seeds in ${Date.now() - startTime} ms` });
             this.exit();
         }
         catch (error) {
-            this.errorHandler.captureException(error);
+            // await to guarantee the error gets captured before the process exits
+            await this.errorHandler.captureException(error);
             this.exit();
         }
     }
